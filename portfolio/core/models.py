4b67bfe093eee52e10b798e9046cc626eea8adf5
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-06-04 18:41:22 -0300

    Configuração do workspace junto de pequenas alterações

--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
+
 class UsuarioModel(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=100)
-    idade = models.IntegerField()
+    nome = models.TextField(
+        verbose_name="Nome", max_length=100, null=False, blank=False
+    )
+    email = models.EmailField(verbose_name="Email", null=False, blank=False)
+    senha = models.TextField(verbose_name="Senha", null=False, blank=False)
 
     def __str__(self):
         """Devolve uma representação em string do modelo."""
         return self.nome
 
     class Meta:
-        verbose_name = 'Usuario'
-        verbose_name_plural = 'Usuarios'
-    
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+        ordering = ["nome"]
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='index'),
-    path('usuario/cadastro', views.CadastroView.as_view(), name='cadastro'),
-    path('usuario/login', views.LoginView.as_view(), name='login'),
+    path("admin/", admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
+    path("usuario/cadastro", views.CadastroView.as_view(), name="cadastro"),
+    path("usuario/login", views.LoginView.as_view(), name="login"),
 ]
